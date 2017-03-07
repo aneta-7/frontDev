53732e82114c9164ef0192876ff1d1ce1e6b0b46
aneta-7 <aneta.stypa7@wp.pl>
2017-03-07 19:15:39 +0100

    first project

---
+++ proj1/data.js
@@ -0,0 +1,5 @@
+var data = [
+    {"title":"Human", "performer":"Rag'n'Bone Man", "publishing":"Sony Music Entertainment", "type":"Pop & Rock", "year":"2017"},
+    {"title":"Chodź pomaluj mój świat", "performer":"Dwa plus jeden", "publishing":"Warner Music Poland", "type":"Pop & Rock", "year":"1970"},
+    {"title":"Gwaizda", "performer":"Peszek Maria", "publishing":"Warner Music Poland", "type":"Alternative", "year":"2016"}
+]
