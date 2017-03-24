28d4e15aa8015887e707e3d1cae8063243a2b1e3
aneta-7 <aneta.stypa7@wp.pl>
2017-03-24 19:45:22 +0100

    delete comment code

--- zad2/main.js
+++ zad2/main.js
@@ -16,58 +16,42 @@ class Song {
         }
     }
 
-        toTableRow(position){
-            return `<td><tr>${position.title} </tr> 
-                <tr>${position.performer}  </tr>
-                <tr>${position.publishing} </tr>
-                <tr>${position.type}       </tr>
-                <tr>${position.year}       </tr></td>`;
-        }
+    toTableRow(position){
+        return `<td><tr>${position.title} </tr> 
+            <tr>${position.performer}  </tr>
+            <tr>${position.publishing} </tr>
+            <tr>${position.type}       </tr>
+            <tr>${position.year}       </tr></td>`;
+    }
 
-        generateTableHeader(){
-            return `<tr><th>Title</th> <th>Performer</th> <th>Publishing</th> <th>Type</th> <th>Year</th></tr>`;
-        }  
+    generateTableHeader(){
+        return `<tr><th>Title</th> <th>Performer</th> <th>Publishing</th> <th>Type</th> <th>Year</th></tr>`;
+    }  
 
-        addPosition(json) {
-            let position = [];
-            position.push(new Song(json));
-            return position;
-        }
-        
-        toTable(position){ 
+    addPosition(json) {
+        let position = [];
+        position.push(new Song(json));
+        return position;
+    }
+    
+    toTable(position){ 
         let row;
         let table = `<table border=1 frame=hsides rules=columns>`;
         table  += this.generateTableHeader();
-// position.forEach(row => {
-//             row = `<td><tr>${row[0].title} </tr> 
-//                 <tr>${row[0].performer}  </tr>
-//                 <tr>${row[0]n.type}       </tr>
-//                 <tr>${position.year}       </tr></td>`;
-//            // table += this.toTableRow(position[i]);
-//            table += row;
-//         })
+
         position.forEach(row => {
             table += row[0].toTableRow(row);
 
         });
-        // for (let i = 0; i<position.length; i++){
-        //     let pos = position[i];
-        //     table += pos[0].toTableRow(pos);
-         
-        // }
+
         table +=`</table>`
         return table;
-        }
+    }
 
-        init(){
-            let position = [];
-            data.forEach(song => position.push(this.addPosition(song)));
-            // for(let i = 0; i<data.length; i++){
-            //     //listOfSongs.addPosition(data[i]);
-            //     position.push(this.addPosition(data[i]));
-            // }
-            let context = document.getElementById('table');
-            context.innerHTML = this.toTable(position);
-        }
-
+    init(){
+        let position = [];
+        data.forEach(song => position.push(this.addPosition(song)));
+        let context = document.getElementById('table');
+        context.innerHTML = this.toTable(position);
+    }
 }
