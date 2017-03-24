b0958139938855ad7480437609cb99e7fa02c5f7
aneta-7 <aneta.stypa7@wp.pl>
2017-03-24 19:42:36 +0100

    zad2

--- zad2/main.js
+++ zad2/main.js
@@ -1,56 +1,73 @@
-function Song(json){
-    let self = this;
-    self.title = json.title;
-    self.performer = json.performer;
-    self.publishing = json.publishing;
-    self.type = json.type;
-    self.year = json.year;
-
-    self.toTableRow = function(){
-        return `<tr><td>
-            ${self.title}        </td><td>
-            ${self.performer}    </td><td>
-            ${self.publishing}   </td><td>
-            ${self.type}         </td><td>
-            ${self.year}         </td></tr>`
-    }
-}
-
-function toTableRow(position){
-
-    return `<td><tr>${position.title} </tr> 
-           <tr>${position.performer}  </tr>
-           <tr>${position.publishing} </tr>
-           <tr>${position.type}       </tr>
-           <tr>${position.year}       </tr></td>`;
-}
-
-var generateTableHeader = function(){
-    return `<tr><th>Title</th> <th>Performer</th> <th>Publishing</th> <th>Type</th> <th>Year</th></tr>`;
-}
-
-function ListOfPosition(){
-    let position = [];
-    let self = this;
-    self.addPosition = function(json) {
-        position.push(new Song(json));
-    }
+class Song {
+    constructor(json){
+        this.title = json.title;
+        this.performer = json.performer;
+        this.publishing = json.publishing;
+        this.type = json.type;
+        this.year = json.year;
     
-    self.toTable = function(){
-        let table = `<table border=1 frame=hsides rules=columns>`;
-        table  += generateTableHeader();
-        for (let i = 0; i<position.length; i++){
-            table +=position[i].toTableRow(position);
+        this.toTableRow = function(){
+            return `<tr><td>
+                ${this.title}        </td><td>
+                ${this.performer}    </td><td>
+                ${this.publishing}   </td><td>
+                ${this.type}         </td><td>
+                ${this.year}         </td></tr>`
         }
+    }
+
+        toTableRow(position){
+            return `<td><tr>${position.title} </tr> 
+                <tr>${position.performer}  </tr>
+                <tr>${position.publishing} </tr>
+                <tr>${position.type}       </tr>
+                <tr>${position.year}       </tr></td>`;
+        }
+
+        generateTableHeader(){
+            return `<tr><th>Title</th> <th>Performer</th> <th>Publishing</th> <th>Type</th> <th>Year</th></tr>`;
+        }  
+
+        addPosition(json) {
+            let position = [];
+            position.push(new Song(json));
+            return position;
+        }
+        
+        toTable(position){ 
+        let row;
+        let table = `<table border=1 frame=hsides rules=columns>`;
+        table  += this.generateTableHeader();
+// position.forEach(row => {
+//             row = `<td><tr>${row[0].title} </tr> 
+//                 <tr>${row[0].performer}  </tr>
+//                 <tr>${row[0]n.type}       </tr>
+//                 <tr>${position.year}       </tr></td>`;
+//            // table += this.toTableRow(position[i]);
+//            table += row;
+//         })
+        position.forEach(row => {
+            table += row[0].toTableRow(row);
+
+        });
+        // for (let i = 0; i<position.length; i++){
+        //     let pos = position[i];
+        //     table += pos[0].toTableRow(pos);
+         
+        // }
         table +=`</table>`
         return table;
-    }
-}
-function init(){
-let listOfSongs = new ListOfPosition();
-    for(let i = 0; i<data.length; i++){
-        listOfSongs.addPosition(data[i]);
-    }
-    let context = document.getElementById('table');
-    context.innerHTML = listOfSongs.toTable();
+        }
+
+        init(){
+            let position = [];
+            data.forEach(song => position.push(this.addPosition(song)));
+            // for(let i = 0; i<data.length; i++){
+            //     //listOfSongs.addPosition(data[i]);
+            //     position.push(this.addPosition(data[i]));
+            // }
+            let context = document.getElementById('table');
+            context.innerHTML = this.toTable(position);
+        }
+
 }
